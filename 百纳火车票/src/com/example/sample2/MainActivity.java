457b91b9531938ebb03de8f60922177e8f8a0698
salalaiji <1109435501@qq.com>
2016-05-29 22:24:23 +0800

    查询页面完成

--- 百纳火车票/src/com/example/sample2/MainActivity.java
+++ 百纳火车票/src/com/example/sample2/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 
 
@@ -25,6 +26,25 @@ public class MainActivity extends Activity {
 	  setContentView(R.layout.activity_main);
 	  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	}
-
+	
+	public void GoToTidSelect(View v)//切换到车次查询页面
+	{
+		setContentView(R.layout.tid_select);
+	}
+	
+	public void GoBack(View v)//返回主页面
+	{
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void GoToStationSelect(View v)
+	{
+		setContentView(R.layout.station_select);//切换到车站查询页面
+	}
+    
+	public void GoToZZSelect(View v)
+	{
+		setContentView(R.layout.zz_select);
+	}
 }
 
