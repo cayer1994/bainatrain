db34365fbc701ee11253196a10189715f6f0968a
salalaiji <1109435501@qq.com>
2016-05-29 21:52:23 +0800

    初步试探布局和页面设置

--- 百纳火车票/src/com/example/sample2/MainActivity.java
+++ 百纳火车票/src/com/example/sample2/MainActivity.java
@@ -1,26 +1,29 @@
 package com.example.sample2;
 
-import android.support.v7.app.ActionBarActivity;
-
 import java.util.ArrayList;
 import java.util.List;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AliasActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 
 
-@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
 	DBMethod dbm;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
+	 super.onCreate(savedInstanceState);
 	  dbm=new DBMethod(this);
-	  
+	  setContentView(R.layout.activity_main);
+	  getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 	}
 
 }
